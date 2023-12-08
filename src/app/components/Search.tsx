d1a450a7e7c9e197858767f0f5833facbdaf5143
Maria <sawaby@users.noreply.github.com>
2023-12-08 08:47:13 -0800

    cleaning

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -39,16 +39,7 @@ export default function Search() {
           
         }
         
-         {/* <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-            <div className="relative mt-3 md:hidden">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-                </div>
-                <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-700" placeholder="Search..." />
-            </div>
-        </div> */}
+        
         </>
 
   )
