6196a11edeb15654dd43abedb9bc7d4ec37f4fa6
Maria <sawaby@users.noreply.github.com>
2023-12-21 09:33:40 -0800

    search bar

--- src/app/components/SearchInput.tsx
+++ src/app/components/SearchInput.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { TbSearch, TbMenu } from "react-icons/tb";
 import TagPostList from '../tags/[tag]/page';
+import { useRef } from 'react';
 
+export default function SearchInput() {
+  const inputRef = useRef("");
 
-export default async function SearchInput() {
-  
-  function handleSearch(term: string ) {
+  function handleSearch(e:React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    
+    console.log(inputRef.current.value);
     // <TagPostList tag={term}/>
   }
   return (
-    <form>
+    <form onSubmit={handleSearch}>
         <div className="absolute top-[10px] md:inset-y-0 md:start-0 md:flex items-center ps-3 pointer-events-none">
             <TbSearch size={20} />
             <span className="sr-only">Search icon</span>
         </div>
-        <input type="text" onChange={(e) => {handleSearch(e.target.value);}} className="block w-[237px] p-2 ps-10 text-sm border border-zinc-800 rounded-lg bg-zinc-950/80 focus:outline-zinc-600" placeholder="Search..." />
+        {/* onChange={(e) => {handleSearch(e.target.value);}} */}
+        <input type="text" ref={inputRef} className="block w-[237px] p-2 ps-10 text-sm border border-zinc-800 rounded-lg bg-zinc-950/80 focus:outline-zinc-600" placeholder="Search..." />
     </form>
   )
 }
