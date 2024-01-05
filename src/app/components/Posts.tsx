c4f963d32383901c9c2918c84730001f268b5595
Maria <sawaby@users.noreply.github.com>
2024-01-05 15:11:08 -0800

    local storage

--- src/app/components/Posts.tsx
+++ src/app/components/Posts.tsx
@@ -35,14 +35,14 @@ export default async function Posts() {
   if (!posts) {
     return <p className="mt-6 text-center">No Posts Available.</p>;
   }
-  // console.log(topic)
+  // console.log("posts ", posts)
   return (
     <div className=" text-slate-400">
       
       <SmallScreenSidebar posts={posts}/>
      
       <div className="hidden md:flex h-screen mr-4 pr-10 flex-col md:w-60">
-        <h4 className="text-2xl pb-2 mb-4 font-bold">Topic</h4>
+        {/* <h4 className="text-2xl pb-2 mb-4 font-bold">Topic</h4> */}
         <ul>
           {posts.map((post) => (
             <SideNav key={post.id} post={post} additionalStyle="" />
