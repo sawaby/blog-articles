845069c70acde534034aefeb03e90fa2bcc703e6
Maria <sawaby@users.noreply.github.com>
2024-01-12 09:08:03 -0800

    getting localStorage in server component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -6,9 +6,14 @@ type Props ={
   }
 }
 //  display introduction based on topic *todo
-export default function page({params: {topic}}: Props) {
-  console.log("topic in main post page: ", topic)
+export default function page() {
+  let getData:string ="Python"
+  if (typeof window !== 'undefined'){
+    getData = JSON.parse(localStorage.getItem("topic")!);
+    console.log("topic from page server is: ", getData);
+  }
+  // console.log("topic in main post page: ", topic)
   return (
-    <div className='mt-20 md:mt-auto'></div>
+    <div className='mt-20 md:mt-auto'>{getData}</div>
   )
 }
