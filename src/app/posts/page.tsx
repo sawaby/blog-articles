e961c284f027e1e5036846c7de730c13faf6a808
Maria <sawaby@users.noreply.github.com>
2024-02-02 08:36:25 -0800

    getting localStorage in server component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -8,10 +8,15 @@ type Props ={
 //  display introduction based on topic *todo
 export default function page() {
   let getData:string =""
-  if (typeof window !== 'undefined'){
-    getData = JSON.parse(localStorage.getItem("topic")!);
-    console.log("topic from page server is: ", getData);
-  }
+  // set a delay time to wait for window to be ready Note: this didn't work out, so I have to change it next time.
+  setTimeout(
+    () => {
+      if (typeof window !== 'undefined'){
+        getData = JSON.parse(localStorage.getItem("topic")!);
+        console.log("topic from page server is: ", getData);
+      }
+    }
+    , 2000)
   console.log("topic in main post page: ", getData)
   return (
     <div className='mt-20 md:mt-auto'>{getData}</div>
